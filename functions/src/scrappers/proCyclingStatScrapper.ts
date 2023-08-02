11c5f14be28d4d735e9566cc6829c371d4705f0b
Guillaume Houis <guilaume.houis@gmail.com>
2023-08-02 14:31:26 +0200

    scrap competition years

--- functions/src/scrappers/proCyclingStatScrapper.ts
+++ functions/src/scrappers/proCyclingStatScrapper.ts
@@ -94,6 +94,11 @@ async function getRider(rank: Rank): Promise<Rider | null> {
 
 
     let topResultLine: HTMLLIElement | undefined | null = pageContent?.getElementsByClassName('right').item(0)?.querySelector('li');
+    const years = topResultLine?.innerHTML.match(/\(('\d+,?\s?)+\)/)?.[0]
+        .replace('\'', '')
+        .replace(' ', '')
+        .split(',')
+        .map((year) => 2000 + parseInt(year));
     let infoElement: Element | null | undefined = pageContent?.getElementsByClassName('rdr-info-cont').item(0);
     pageContent = null;
 
@@ -141,7 +146,7 @@ async function getRider(rank: Rank): Promise<Rider | null> {
             raceType: raceType,
             recurrence: parseInt(recurrence),
             rank: parseInt(position),
-            years: []
+            years: years ?? [],
         },
         team: rank.team
     };
